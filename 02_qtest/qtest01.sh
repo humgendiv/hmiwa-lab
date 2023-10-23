4aafdaa62080807dc3019bd1a7c27fbd4dc7e211
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2023-10-23 15:03:40 +0900

    Update qtest01.sh

--- 02_qtest/qtest01.sh
+++ 02_qtest/qtest01.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -N QSUB_TEST
-#PBS -cwd
 #PBS -o ~/hmiwa/99_log/qtest01.log
 #PBS -e ~/hmiwa/99_log/qtest01.log
 
 #qsystem test
+cd `pwd`
 echo "This is a test trial." > output.txt
 echo `date` >> output.txt 
 for i in {1..22}
