0ae05ec6cbf07635a6e02b968af61971b68d584f
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2023-10-23 16:49:09 +0900

    Update qtest02.sh

--- 02_qtest/qtest02.sh
+++ 02_qtest/qtest02.sh
@@ -4,5 +4,12 @@
 #PBS -e qtest02.e
 
 #qsystem test
+echo `date`
 echo `pwd`
 echo $PBS_O_WORKDIR
+
+if [ ! -z ${PBS_O_WORKDIR} ]; then
+  cd ${PBS_O_WORKDIR} # PBS_O_WORKDIR 変数が存在している場合、そのディレクトリへ移動
+fi
+
+echo `pwd`
