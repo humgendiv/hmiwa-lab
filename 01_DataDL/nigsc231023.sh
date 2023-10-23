9cf5b77a127c77b3e0fb94194739df29be0b1c79
hmiwa30 <109275358+hmiwa30@users.noreply.github.com>
2023-10-23 14:28:01 +0900

    Update nigsc231023.sh

--- 01_DataDL/nigsc231023.sh
+++ 01_DataDL/nigsc231023.sh
@@ -4,5 +4,5 @@ cd ~/hmiwa1/rawdata/
 #scp hmiwa-spc:~/hmiwa/downloads/XXXXX ~/hmiwa1/rawdata/XXXXX
 
 XXXXX=""
-scp hmiwa-spc:~/hmiwa/downloads/${XXXXX} ${XXXXX}
-tar xvzf ${XXXXX}
+scp hmiwa-spc:~/hmiwa/downloads/${XXXXX}.tar.gz ${XXXXX}.tar.gz
+tar xvzf ${XXXXX}.tar.gz
