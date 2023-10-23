40da1b7b7cf51d038bb995ca8d5fe923631be583
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2023-10-23 18:06:00 +0900

    Update qtest06.sh

--- 02_qtest/qtest06.sh
+++ 02_qtest/qtest06.sh
@@ -2,22 +2,15 @@
 #PBS -j oe 
 #PBS -o /home/hmiwa/hmiwa/hmiwa-labserver/99_log/qtest06.log
 #PBS -e /home/hmiwa/hmiwa/hmiwa-labserver/99_log/qtest06.log
-#PBS -N $JN
 
-#qsystem test
-echo `date`
-echo `pwd`
-
-echo $PBS_O_WORKDIR
+sshpass=~/homebrew/bin/sshpass
 
 if [ ! -z ${PBS_O_WORKDIR} ]; then
   cd ${PBS_O_WORKDIR} # PBS_O_WORKDIR 変数が存在している場合、そのディレクトリへ移動
 fi
-echo `pwd`
 
-#qsub -v pwd=`pwd` qtest01.sh
-
+$sshpass -p $PASS scp hmiwa-spc:~/hmiwa/${FILENAME} ${FILENAME}
 #scp hmiwa-spc:~/hmiwa/downloads/XXXXX ~/hmiwa1/rawdata/XXXXX
 
-XXX=""
-scp hmiwa-spc:~/hmiwa/${XXX} ${XXX}
+#FILENAME=tmp.sh
+#qsub -N SCP_$FILENAME -v FILENAME=$FILENAME -v PASS=... qtest01.sh
