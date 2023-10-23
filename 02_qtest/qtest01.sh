ddee58cf6c2e7d314849ad8b7a220c9f6af5f790
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2023-10-23 15:12:52 +0900

    Update qtest01.sh

--- 02_qtest/qtest01.sh
+++ 02_qtest/qtest01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N QSUB_TEST
-#PBS -o ~/hmiwa/hmiwa_labserver/99_log/qtest01.log
-#PBS -e ~/hmiwa/hmiwa_labserver/99_log/qtest01.log
+#PBS -o qtest01.log
+#PBS -e qtest01.log
 
 #qsystem test
 #qsub -v pwd=`pwd` qtest01.sh
