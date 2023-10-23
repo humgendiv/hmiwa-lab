49caeb31b2996c52f84648d04c2698ebebea6ee6
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2023-10-23 15:10:33 +0900

    Update qtest01.sh

--- 02_qtest/qtest01.sh
+++ 02_qtest/qtest01.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #PBS -N QSUB_TEST
-#PBS -o ~/hmiwa/99_log/qtest01.log
-#PBS -e ~/hmiwa/99_log/qtest01.log
+#PBS -o ~/hmiwa/hmiwa_labserver/99_log/qtest01.log
+#PBS -e ~/hmiwa/hmiwa_labserver/99_log/qtest01.log
 
 #qsystem test
-cd `pwd`
-echo "This is a test trial." > output.txt
-echo `date` >> output.txt 
+#qsub -v pwd=`pwd` qtest01.sh
+OUTFILE=$pwd/output.txt
+echo "This is a test trial." > $OUTFILE
+echo `date` >> $OUTFILE
 for i in {1..22}
 do
-echo "count $i" >> output.txt
+echo "count $i" >> $OUTFILE
 done
