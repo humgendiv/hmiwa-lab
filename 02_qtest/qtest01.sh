dcadc91bc38a3bc6d3f2753a35ef2bc42a62fe20
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2023-10-23 14:56:26 +0900

    Update qtest01.sh

--- 02_qtest/qtest01.sh
+++ 02_qtest/qtest01.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#PBS -N QSUB_TEST
+#PBS -cwd
+#PBS -o ~/hmiwa/99_log/qtest01.log
+#PBS -e ~/hmiwa/99_log/qtest01.log
+
 #qsystem test
 echo "This is a test trial." > output.txt
 echo `date` >> output.txt 
