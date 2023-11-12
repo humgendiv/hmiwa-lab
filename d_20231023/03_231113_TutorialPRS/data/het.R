451c08519340d19954aa72064e199f89e82d0c47
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2023-11-13 03:11:11 +0900

    [add] try another PRS tutorial

---
+++ d_20231023/03_231113_TutorialPRS/data/het.R
@@ -0,0 +1,5 @@
+dat <- read.table("EUR.QC.het", header=T) # Read in the EUR.het file, specify it has header
+m <- mean(dat$F) # Calculate the mean
+s <- sd(dat$F) # Calculate the SD
+valid <- subset(dat, F <= m+3*s & F >= m-3*s) # Get any samples with F coefficient within 3 SD of the population mean
+write.table(valid[,c(1,2)], "EUR.valid.sample", quote=F, row.names=F) # print FID and IID for valid samples
